1760a813fb2abbb5d654757d2cecd3c0f6c20c3b
Tammie Chung <tammie.chung11@gmail.com>
2020-05-05 19:47:51 -0400

    Add setting options

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const fetchImage = () => {
 
   axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit: 1, size: 'full' } })
     .then((res) => {
-      console.log('response:', res)
 
       if (res.status !== 200) {
         // handle client side error message for if status is anything other than 200 but not a request error
@@ -13,8 +12,6 @@ const fetchImage = () => {
         if (img === undefined) {
           // error handling for no image result
         } else {
-          // image exists - now what??
-          console.log('img:', img);
           const image = document.createElement('img');
           const container = document.getElementById('photo-container');
           image.src = img;
@@ -26,17 +23,84 @@ const fetchImage = () => {
       // handle request error here
       console.log('Fetch Error:', err);
     });
-
 };
 
 window.onload = () => {
+
+  // fetch image on load
   fetchImage();
 
-  const getNewKittyButton = document.getElementById('new-kitty-pls');
-
-  getNewKittyButton.addEventListener('click', () => {
-    const oldImg = document.getElementsByTagName('img')[0];
-    oldImg.remove();
+  // get a new kitty on button click
+  document.getElementById('new-kitty-pls').addEventListener('click', () => {
+    document.getElementsByTagName('img')[0].remove();
     fetchImage();
   });
+
+  // username variables
+  const storedUserName = localStorage.getItem('receivedName');
+  let userName =  storedUserName ? storedUserName : 'Friend';
+
+  // get initial greeting
+  const getGreeting = () => {
+    document.getElementById("greeting").innerHTML  = `Hello, ${userName}. Here's a cute kitty for you!`;
+  };
+ 
+  getGreeting();
+
+  // change and update name
+  const saveName = () => {
+    localStorage.setItem('receivedName', userName);
+  };
+
+  const changeName = () => {
+    userName = document.getElementById("name-input").value;
+    saveName();
+    getGreeting();
+  };
+
+  document.getElementById("name-form").addEventListener('submit', function(e) {
+    e.preventDefault();
+    changeName();
+  });
+
+  // toggle settings
+  const toggleSettings = () => {
+    document.getElementById("settings").classList.toggle("settings-open");
+  };
+
+  document.getElementById("settings-button").addEventListener('click', toggleSettings);
+
+  // colour picker settings
+  const storedColor = localStorage.getItem('receivedColor');
+  let color =  storedColor ? storedColor : '#FFFFF';
+
+  const setColor = () => {
+    console.log('color:', color)
+    const body = document.getElementsByTagName('body')[0];
+    const settings = document.getElementById('settings-button-container');
+    body.setAttribute('style', `background-color: ${color};`);
+    settings.setAttribute('style', `background-color: ${color};`);
+  };
+ 
+  setColor();
+
+  const saveColor = () => {
+    localStorage.setItem('receivedColor', color);
+  };
+
+  const changeColor = (buttonColor) => {
+    color = buttonColor;
+    saveColor();
+    setColor();
+  };
+
+  const colorButtons = document.getElementsByClassName('color-button');
+
+  for (let button of colorButtons) {
+    console.log('button:', button)
+    button.addEventListener('click', (e) => {
+      console.log('e:', e)
+      changeColor(e.target.value);
+    });
+  }
 };
